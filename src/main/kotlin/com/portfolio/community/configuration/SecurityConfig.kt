2b0d566bcb252318ace9ff50b3218befc7ca99b4
YJ-1412 <ykjeong6@naver.com>
2024-09-24 01:30:20 +0900

    /swagger-ui/** cors 비활성화 테스트

--- src/main/kotlin/com/portfolio/community/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/portfolio/community/configuration/SecurityConfig.kt
@@ -13,6 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -48,4 +52,14 @@ class SecurityConfig(
         return auth.build()
     }
 
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/swagger-ui/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
 }
