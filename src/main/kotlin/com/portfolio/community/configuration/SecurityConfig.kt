fb440efe1d74a4e75cb91183ef86e729eea0a850
YJ-1412 <ykjeong6@naver.com>
2024-09-24 13:53:03 +0900

    swagger 자원 spring security에서 무시하도록 설정

--- src/main/kotlin/com/portfolio/community/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/portfolio/community/configuration/SecurityConfig.kt
@@ -9,7 +9,9 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -54,4 +56,12 @@ class SecurityConfig(
         return auth.build()
     }
 
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring().requestMatchers(
+                "/swagger-ui/**", "/v3/api-docs/**"
+            )
+        }
+    }
 }
