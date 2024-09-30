a906c6c0649ab32f4f28c5f29801c7a677431b9b
YJ-1412 <ykjeong6@naver.com>
2024-09-30 22:44:06 +0900

    Swagger JWT 인증 테스트

--- src/main/kotlin/com/portfolio/community/configuration/OpenApiConfig.kt
+++ src/main/kotlin/com/portfolio/community/configuration/OpenApiConfig.kt
@@ -1,20 +1,25 @@
 package com.portfolio.community.configuration
 
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
-import org.springframework.context.annotation.Bean
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig {
-    @Bean
-    fun customOpenAPI(): OpenAPI {
-        return OpenAPI()
-            .info(
-                Info()
-                    .title("Community API")
-                    .version("1.0")
-                    .description("백엔드를 공부하기 위하여 만드는 커뮤니티 API 프로젝트")
-            )
-    }
-}
+@OpenAPIDefinition(
+    info = Info(
+        title = "Community API",
+        version = "1.0",
+        description = "백엔드를 공부하기 위하여 만드는 커뮤니티 API 프로젝트",
+    ),
+    security = [SecurityRequirement(name = "Bearer Authentication")]
+)
+@SecurityScheme(
+    name = "Bearer Authentication",  // 인증 스키마 이름
+    type = SecuritySchemeType.HTTP,  // HTTP 기반 인증
+    scheme = "bearer",               // Bearer 토큰 방식
+    bearerFormat = "JWT"             // JWT 형식 명시
+)
+class OpenApiConfig
